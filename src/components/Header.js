85f21373a926145a1eeb18ad55d7dab2babe372a
Sofia Darke <sofia.darke@hyperisland.se>
2021-02-23 12:39:14 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+  title: "Ricola",
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+//alternativt sätt
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//     </div>
+//   )
+// }
+
+// export default App
