87168e396a3f1409e5715f5594e2ea432a811369
Sofia Darke <sofia.darke@hyperisland.se>
2021-03-04 15:16:12 +0100

    Co-authored-by: EphraimVC <EphraimVC@users.noreply.github.com>

---
+++ src/components/Toptext.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import styles from "./Toptext.module.css"
+
+const Toptext = () => {
+  return (
+    <div>
+      <h3 /*className={styles.header}*/>Ricola text about nature</h3>
+    </div>
+  )
+}
+
+// Header.defaultProps = {
+//   title: "Ricola",
+// }
+
+export default Toptext
