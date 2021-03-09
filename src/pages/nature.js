189c9e8fd1d6602b42ad0f8753d76e0c8442ab8c
Sofia Darke <sofia.darke@hyperisland.se>
2021-03-09 14:31:14 +0100

    pop styling start / navbar added

--- src/pages/nature.js
+++ src/pages/nature.js
@@ -5,11 +5,13 @@ import Footer from "../components/Footer"
 import NatureCards from "../components/NatureCards"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Toptext from "../components/Toptext"
+import Navbar from "../components/Navbar"
 // import * as ReactBootStrap from "react-bootstrap"
 
 export default function App() {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <Toptext />
       <NatureCards />
